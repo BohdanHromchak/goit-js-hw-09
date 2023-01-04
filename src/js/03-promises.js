626504b066990a2d37ec97c309b4b16748e388eb
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2023-01-04 15:33:26 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,37 @@
 import Notiflix from 'notiflix';
 // console.log(Notiflix);
 
+const form = document.querySelector(".form");
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  for (let i = 0; i < Number(amount.value); i += 1) {
+    createPromise(i + 1, Number(step.value) * i + Number(delay.value))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
-
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-});
